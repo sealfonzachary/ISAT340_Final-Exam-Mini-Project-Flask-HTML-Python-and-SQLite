65c292c778396df41f492b3f0d6b9ef267fde3a2
sealfonzachary <sealfon.zachary@gmail.com>
2025-12-03 10:15:42 -0500

    Add files via upload
    updated

--- celebrities.py
+++ celebrities.py
@@ -2,13 +2,13 @@ import sqlite3
 
 conn = sqlite3.connect("celebrities.db")
 c = conn.cursor()
-'''
+
 c.execute("""
 CREATE TABLE IF NOT EXISTS celebs (
-    celebID integer PRIMARY KEY,
+    celebID text PRIMARY KEY,
     firstname text,
     lastname text,
-    age integer,
+    age text,
     email text,
     photo text,
     bio text
@@ -17,10 +17,10 @@ CREATE TABLE IF NOT EXISTS celebs (
 
 c.execute("""
 CREATE TABLE IF NOT EXISTS members (
-    memberID integer PRIMARY KEY,
+    memberID text PRIMARY KEY,
     firstname text,
     lastname text,
-    age integer,
+    age text,
     email text,
     bio text
 )
@@ -39,17 +39,10 @@ data = [
     (7, "Selena", "Gomez", 23, "selena@hollywood.us", "https://s3.amazonaws.com/isat3402021/sg.jpg",""),
     (8, "Stephen", "Curry", 27, "steph@golden.bb", "https://s3.amazonaws.com/isat3402021/sc.jpg", "")
     ]
-#c.executemany(sql,data)
+c.executemany(sql,data)
 
 conn.commit()
 
-'''
-#TO TEST
-c.execute("SELECT * FROM members")
-rows = c.fetchall()
 
-for row in rows:
-    print(row)
 
-conn.close()
 
